56f633db04c15458f4455a75573dcaf4784962a0
Vladislav Likhachev <mymindissharp@gmail.com>
2022-04-19 21:15:33 +0300

    initial commit

---
+++ src/test/java/com/returtless/javadiplom/JavadiplomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.returtless.javadiplom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadiplomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
